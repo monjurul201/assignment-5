898772a1fcb0050099d78de48713843c7300dfc6
unknown <mojnushah2018@gmai.com>
2021-02-08 14:28:39 +0600

    hungry monstar api complete

--- app.js
+++ app.js
@@ -1,162 +1,74 @@
 
-const searchInput = document.getElementById('search-box');
-const SearchBtn = document.getElementById('search-btn');
-const showMeals = document.getElementById('meals');
-const resultHeading = document.getElementById('result-heading');
-const singleMeal = document.getElementById('single-meal');
-const searchResult = document.getElementById('search-result')
-
-SearchBtn.addEventListener('click', () => {
-    searchMeal();
-    searchInput.value = "";
-})
-
-function searchMeal() {
-    const searchMeals = searchInput.value;
-    if (searchMeals.trim()) {
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeals}`)
-            .then(response => response.json())
-            .then(data => {
-
-                console.log(data);
-                searchResult.innerHTML = `<h2 class="searchResult">search result for : ${searchMeals}</h2>`;
-                if (data.meals === null) {
-                    searchResult.innerHTML = `
-                    <p class="searchResult">There Is No Search For This Meal</p>
-                    `;
-                } else {
-                    showMeals.innerHTML = data.meals.map((meal) => `
-
-                        <div class="meal">
-                            <div class="show-details">
-                            <img  src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
-                            </div>
-                            <div class="meal-info" data-mealID="${meal.idMeal}">
-                                <h4>${meal.strMeal}</h4>
-                            </div>
-                        </div>
-                        
-                        `
-                    )
-                  .join("");
-                }
-            });
+const searchMeals = () => {
+    const searchInput = document.getElementById('search-box').value;
+    // console.log(searchInput);
+    if(searchInput===""){
+        alert('At first type value input box then click search button')
     }
-     else {
-        alert("Please Write Something input Box.");
-    };
+    else{
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`
+ 
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayMeals(data.meals))
+    }
+    
+
 }
 
+const displayMeals = meals => {
+    const mealContainer = document.getElementById('meals');
+    meals.forEach(meal => {
+        const singleMeal = document.createElement('div');
+        singleMeal.className = 'single-meal';
+        singleMeal.innerHTML = `  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const apiKey='870967436c1517d581daf3b245495790'
-const displayArea = document.getElementById('show-movies');
-const singleDisplayArea = document.getElementById('singleMovie');
-
-
-function getPopularMovies() {
-    fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`)
-    .then(res => res.json())
-    .then(data => {
-
-        let previousHTML = displayArea.innerHTML;
-
-        data.results.slice(0, 12)
-        .map(movie => {
-            const htmlTemplate = `
-            <div class="col">
-                    <div class="card">
-                    <img src="https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}" alt="${movie.title}">
-                    <div class="card-body">
-                        <h5 class="card-title">${movie.title}</h5>
-                        <button onclick="movieDetailsView(${movie.id})" class="btn btn-outline-success">View Details</button>
-                    </div>
-                    </div>
+            <div onclick=" showDetails('${meal.strMeal}')" class="show-details">
+            <img  src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
             </div>
-            `;
-            previousHTML += htmlTemplate;
+            <div class="meal-info" data-mealID="${meal.idMeal}">
+                <h4>${meal.strMeal}</h4>
+            </div>              
+        `;
+
+        mealContainer.appendChild(singleMeal);
+    })
+
+}
+
+   showDetails = (searchInput) => {
+       console.log(searchInput);
+    
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+
+            const dataMeal = data.meals[0];
+            console.log(dataMeal);
+
+            const showDataInfo = document.getElementById('showMealList');
+            const mealInfo = `
+            <div class="detailsInfo">
+                <div> 
+                    <img src="${dataMeal.strMealThumb}">
+                    <h3 style="color:white;">${dataMeal.strMeal}</h3>
+                </div>               
+                <ul class="Ingredient" style="color:white">
+                    <li><i class="far fa-square"></i> ${dataMeal.strIngredient1}</li>
+                    <li> <i class="far fa-square"></i>${dataMeal.strIngredient2}</li>
+                    <li><i class="far fa-square"></i> ${dataMeal.strIngredient3}</li>
+                    <li><i class="far fa-square"></i> ${dataMeal.strIngredient4}</li>
+                    <li> <i class="far fa-square"></i>${dataMeal.strIngredient5}</li>
+                    <li><i class="far fa-square"></i> ${dataMeal.strIngredient6}</li>
+                </ul>
+
+            </div>
+                       
+           `;        
+            showDataInfo.innerHTML = mealInfo;           
         })
 
-        displayArea.innerHTML = previousHTML;
-
-    })
-    .catch(err => console.log(err.message))
-}
-
-
-function movieDetailsView(id) {
-    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`)
-    .then(res => res.json())
-    .then(data => {
-       console.log(data);
-
-       displayArea.classList.add('d-none')
-       singleDisplayArea.classList.remove('d-none');
-
-       const htmlTemplate = `
-        <div class="card">
-        <img src="https://image.tmdb.org/t/p/w1280/${data.backdrop_path}" alt="${data.original_title}">
-            <div class="card-body">
-            <h5 class="card-title">${data.original_title}</h5>
-
-            <p class="card-text">${data.overview}</p>
-            </div>
-        </div>`
-        singleDisplayArea.innerHTML = htmlTemplate;
-    })
-    .catch(err => console.log(err.message));
-
-
 
 }
-
-getPopularMovies()
-*/
